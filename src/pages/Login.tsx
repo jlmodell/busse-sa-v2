1da8635ec003372b1e73f1dfec8cfdcc3ff3457a
Jeff <modell.jeff@me.com>
2020-03-04 08:06:46 -0500

    updated login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,7 +8,7 @@ interface Props extends RouteComponentProps{}
 const Login: React.FC<Props> = observer(({ history }) => {        
     return (
         <div className="flex justify-center items-center m-48">
-            <div className="w-full max-w-md bg-white" >
+            <div className="w-full max-w-md bg-white rounded-md" >
                 <form className="bg-white shadow-md rounded px-8 py-8 pt-8">
                     <div className="px-4 pb-4">
                         <label htmlFor="email" className="text-sm block font-bold  pb-2">EMAIL ADDRESS</label>
